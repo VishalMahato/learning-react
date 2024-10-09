6f431c5aa29c26622b7ba4600f141783f08494d0
Vishal <vishal.mahato@hcltech.com>
2024-10-09 17:14:55 +0530

    files of two way binding

--- two-way-binding/src/App.jsx
+++ two-way-binding/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	let [name, setName] = useState("");
+  
+const submitHandler =function (e) {
+	e.preventDefault();
+	setName("");
 }
 
-export default App
+
+	return (
+		<>
+			<form
+				className="flex gap-2 justify-center items-center w-screen"
+				onSubmit={(e) => submitHandler(e)}
+			>
+				<input
+					className="py-3 rounded-md text-center"
+					type="text"
+					placeholder="Enter Name"
+					onChange={(e) => setName(e.target.value)}
+					value={name}
+				/>
+				<button>Submit</button>
+			</form>
+		</>
+	);
+}
+export default App;
